d29af10a8d0d5fec12dc33f29babc68417adb28d
Mark Chester <mark@chesterfamily.org>
2014-09-06 23:58:06 -0700

    Final draft UNTESTED

--- AC_Dimmer.ino
+++ AC_Dimmer.ino
@@ -31,15 +31,15 @@ int dimmerKnob[4];                           // The dimmer input value array
 unsigned long dimmerKnobReadTime;            // When it's time to read the dimmer knob inputs in micros()
 
 // Triacs
+const byte triacPin[4] = {4,7,8,12};  // Digital IO pins used for the triac gates
 unsigned long triacNextFireTime[4];   // Timestamp in micros() when it's OK to fire the triacs again.
-const byte triacPin[4] = {4,7,8,12};  // Which digital IO pins are used for the triac gate
 const int triacPulsePeriod = 10;      // How long of a pulse duration is required to properly fire the triacs, in micros() (this is very approximate)
 
 // Initialize things
 void setup() {
-  // Attach the zero-cross interrupt (verify which 'mode' parameter works best with the zero-cross sensor you use)
-  attachInterrupt(zeroCrossInt, zeroCrossDetect, FALLING);  
-  pinMode(triacPin[0], OUTPUT);                             // Set the Triac pin as output
+  // Attach the zero-cross interrupt (verify which 'mode' parameter works best with the zero-cross detector you use)
+  attachInterrupt(zeroCrossInt, zeroCrossDetect, FALLING);
+  pinMode(triacPin[0], OUTPUT);  // Set the Triac pin as output
   pinMode(triacPin[1], OUTPUT);
   pinMode(triacPin[2], OUTPUT);
   pinMode(triacPin[3], OUTPUT);
@@ -48,7 +48,7 @@ void setup() {
 // Main Loop
 void loop() {
   // Keep these three grouped together and put other code above or below them.
-  // There is a certain need for good timing, so you can't use too much other code around these.
+  // There is a certain need for good timing, so you can't use too much other code on a 328p/16MHz.
   // Use a faster board such as the Due if you need to do a lot of other stuff.
   readDimmerKnobs();
   checkZeroCross();
@@ -73,17 +73,19 @@ void readDimmerKnobs(){
 
 // Act on a zero-cross detection
 void checkZeroCross() {
-  if ( zeroCross ) {                                                                    // Have we detected a zero cross?
-    zeroCrossTime = micros();                                                           // Set the zero cross time in micros()
-    zeroCross = false;                                                                  // Reset the flag
-    triacNextFireTime[0] = zeroCrossTime + map(dimmerKnob[0],0,1023,delayPeriod,long(linePeriod)); // Calc the next triac fire time
+  if ( zeroCross ) {           // Have we detected a zero-crossing?
+    zeroCrossTime = micros();  // Set the zero cross time in micros()
+    zeroCross = false;         // Reset the flag
+    triacNextFireTime[0] = zeroCrossTime + map(dimmerKnob[0],0,1023,delayPeriod,long(linePeriod)); // Calculate the next triac fire time
     triacNextFireTime[1] = zeroCrossTime + map(dimmerKnob[1],0,1023,delayPeriod,long(linePeriod));
     triacNextFireTime[2] = zeroCrossTime + map(dimmerKnob[2],0,1023,delayPeriod,long(linePeriod));
     triacNextFireTime[3] = zeroCrossTime + map(dimmerKnob[3],0,1023,delayPeriod,long(linePeriod));
   }
 }
 
-// Fire each of the triacs at the right time
+// Fire each of the triacs at the right time.
+// When two or more dimmer knobs are near the same value,
+// they will content for timing and may be jumpy.
 void fireTriacs() {
   if ( micros() >= triacNextFireTime[0] ) {  // Is it time to fire the triacs?
     digitalWrite(triacPin[0], HIGH);         // Fire the Triac to turn on flow of electricity
@@ -97,7 +99,7 @@ void fireTriacs() {
   if ( micros() >= triacNextFireTime[3] ) {
     digitalWrite(triacPin[3], HIGH);
   }
-  digitalWrite(triacPin[0], LOW);                           // Turn off the triac gate (Triac will not actually turn off until ~next zero-cross)
+  digitalWrite(triacPin[0], LOW);  // Turn off the triac gate (Triac will not actually turn off until ~next zero-cross)
   digitalWrite(triacPin[1], LOW);
   digitalWrite(triacPin[2], LOW);
   digitalWrite(triacPin[3], LOW);
